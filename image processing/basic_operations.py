b024f9757aac2ebe05d3da787485cece347ff56f
Aarush Vats <142438175+A3V1@users.noreply.github.com>
2024-12-28 00:34:55 +0530

    Update basic_operations.py

--- image processing/basic_operations.py
+++ image processing/basic_operations.py
@@ -3,3 +3,9 @@ from PIL import Image
 
 def rotate_image(image, angle):
     return image.rotate(angle)
+    
+def resize_image(image,dimensions):
+    return image.resize(dimensions)
+
+def crop_image(image, box):
+    return image.crop(box)
