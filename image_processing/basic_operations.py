ab69f7c704d5cda263afa25b383affdb94ea51b0
kanishka <kanishkapatil227@gmail.com>
2025-01-02 21:07:17 +0530

    changes

--- image_processing/basic_operations.py
+++ image_processing/basic_operations.py
@@ -3,3 +3,12 @@ from PIL import Image
 
 def rotate_image(image, angle):
     return image.rotate(angle)
+
+def crop_image(image):
+    return 
+
+def resize_image(image):
+    return 
+
+def flip_image(image):
+    return
---
+++ image_processing/color_adjustments.py
@@ -0,0 +1,13 @@
+# image_processing/filters.py
+from PIL import Image
+
+def brightness(image):
+    return 
+
+def contrast(image):
+    return 
+
+def saturation(image):
+    return 
+
+
